2633e652168a47062f028f5e504461854d07f683
RupaliD <rupali.dinde@inflectionzone.com>
2023-03-21 09:42:06 +0530

    Fixed name update issue

--- src/routes/users/[userId]/home/+page.server.ts
+++ src/routes/users/[userId]/home/+page.server.ts
@@ -1,30 +1,19 @@
+import type { PageServerLoad} from './$types';
+import type { RequestEvent } from '@sveltejs/kit';
+import { getUserById} from '../../../../routes/api/services/user';
 
-import type { PageServerLoad } from "./$types";
-import {redirect } from '@sveltejs/kit';
-import { SessionManager } from '../../../api/session.manager';
+/////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////
-
-export const load: PageServerLoad = async (event) => {
-
-    const sessionId = event.cookies.get('sessionId');
-    console.log('sessionId', sessionId);
-    const session = await SessionManager.getSession(sessionId);
-    if (!session) {
-        throw redirect(307, '/sign-in');
-    }
-    console.log(`session received`);
-
-    const sessionUser = {
-            sessionId      : session.sessionId,
-            userId         : session.userId,
-            email          : session.email,
-            username       : session.username,
-            profileImageUrl: session.profileImageUrl,
-            fullName       : session.fullName,
-            firstName      : session.firstName,
-            roleId         : session.roleId,
-            age            : session.age
-        };
-    return sessionUser;
+export const load: PageServerLoad = async (event: RequestEvent) => {
+  const sessionId = event.cookies.get('sessionId');
+  console.log('sessionId', sessionId);
+    const userId = event.params.userId;
+    const _user = await getUserById(sessionId, userId);
+    const user = _user.Patient;
+    console.log('user',user);
+    return {
+      user,
+      sessionId,
+    };
 };
+
--- src/routes/users/[userId]/home/+page.svelte
+++ src/routes/users/[userId]/home/+page.svelte
@@ -8,9 +8,10 @@
 	import { LocalStorageUtils } from '$lib/utils/local.storage.utils';
 	import { draggable } from '@neodrag/svelte';
 	let show = true;
+
 	let userId = $page.params.userId;
 	export let data: PageServerData;
-
+	let firstName = data.user.User.Person.FirstName;
 	let learningHomeLink;
 	let myProfileLink;
 	let askSnehaLink;
@@ -38,7 +39,6 @@
 	};
 
 	onMount(() => {
-		userId = data.userId;
 		learningHomeLink = `/users/${userId}/my-learnings`;
 		myProfileLink = `/users/${userId}/my-profile`;
 		askSnehaLink = `/users/${userId}/ask-sneha`;
@@ -56,7 +56,7 @@
 </script>
 
 <div class="grid gird-cols justify-center items-center ">
-	<div class="max-[425px]:w-full w-[375px] h-[812px] ">
+	<div class="max-[425px]:w-full max-[425px]:h-full w-[375px] h-[812px]">
 		<div class="flex  items-center justify-center">
 			{#if $navbarDisplay}
 			<Navbar
@@ -83,7 +83,7 @@
 								alt=""
 							/>
 						</div>
-						<h1 class="card-title text-base-100 text-3xl">Hi {data.firstName}!</h1>
+						<h1 class="card-title text-base-100 text-3xl">Hi {firstName}!</h1>
 						<p class=" text-base-100">
 							Welcome to Raahi, your friend, companion and guide as you navigate your journey into
 							adulthood.
@@ -112,7 +112,7 @@
 						disabled: false,
 						applyUserSelectHack: true,
 					}}
-						class="card card-compact bg-base-100 card-bordered max-[425px]:border-none border-slate-200 rounded-none rounded-t-[44px] shadow-none"
+						class="card card-compact bg-white card-bordered max-[425px]:border-none border-slate-200 rounded-none rounded-t-[44px] shadow-none"
 					>
 					<div class="flex justify-center">
 						<button class=" h-[5px] w-[73px] bg-[#e3e3e3] mt-4 rounded handle" />
