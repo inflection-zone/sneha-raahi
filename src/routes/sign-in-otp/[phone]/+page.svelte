d5597645c47801d2648ba9a8f3c3d01227ac0504
RupaliD <rupali.dinde@inflectionzone.com>
2023-03-21 09:40:24 +0530

    Fixes for background color

--- src/routes/sign-in-otp/[phone]/+page.svelte
+++ src/routes/sign-in-otp/[phone]/+page.svelte
@@ -126,7 +126,7 @@
 <div class="flex items-center justify-center lg:mt-16 md:mt-16 sm:mt-16 mt-0">
 	<div
 		class="card card-compact rounded-none card-bordered max-[425px]:border-none border-slate-400 max-[425px]:w-full w-[375px]
-	h-[812px] bg-base-100  shadow-none "
+	h-[812px] bg-white shadow-none "
 	>
 		<div class="flex items-center justify-center">
 			<img class="mt-12" src="/assets/images/sign-in/svg/logo.svg" alt="" />
